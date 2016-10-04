2466e280bb2c1e23e6a2bb683a4a3a2a7036ea4d
Theophile Kalumbu <theo@art-ev.com>
2016-10-04 22:17:36 +0200

    add tuple month names

--- app/src/Main.elm
+++ app/src/Main.elm
@@ -4,42 +4,89 @@ import Html exposing (..)
 import Html.Attributes exposing (..)
 import Date exposing (Date, Month(..), year, month, day)
 import Date.Extra as Date exposing (Interval(..))
+import Array
+
 
 -- Model
 
 
-type alias Model = {
-  dayPerWeek : Int,
-  selectedMonth: Int,
-  currentDates : List Date
-}
+type alias Model =
+    { selectedMonthIndex : Int
+    , currentDates : List Date
+    }
+
 
 model : Model
-model = {
-  dayPerWeek  = 7
-  ,selectedMonth = 9
-  ,currentDates = monthDates 2016 (Date.Oct)
- }
+model =
+    { selectedMonthIndex = 9
+    , currentDates = monthDates 2016 (Date.Oct)
+    }
+
+
+
+-- Date utils
+
 
 monthDates : Int -> Month -> List Date
 monthDates y m =
-  let
-    start = Date.floor Monday <| Date.fromCalendarDate y m 1
-  in
-    Date.range Day 1 start <| Date.add Day 42 start
+    let
+        start =
+            Date.floor Monday <| Date.fromCalendarDate y m 1
+    in
+        Date.range Day 1 start <| Date.add Day 42 start
+
+
+dayPerWeek : Int
+dayPerWeek =
+    7
+
+
+monthNames : List ( Month, String )
+monthNames =
+    [ ( Jan, "January" )
+    , ( Feb, "February" )
+    , ( Mar, "March" )
+    , ( Apr, "April" )
+    , ( May, "May" )
+    , ( Jun, "June" )
+    , ( Jul, "July" )
+    , ( Aug, "August" )
+    , ( Sep, "September" )
+    , ( Oct, "October" )
+    , ( Nov, "November" )
+    , ( Dec, "December" )
+    ]
+
+
+getMonthNameByIndex : Int -> String
+getMonthNameByIndex selectedMonthIndex =
+    let
+        maybeMonth =
+            Array.fromList monthNames |> Array.get selectedMonthIndex
+    in
+        case maybeMonth of
+            Just date ->
+                date |> snd
+
+            Nothing ->
+                ""
+
+
 
 -- Helpers
 
 
 chunk : Int -> List a -> List (List a)
 chunk n list =
-  if List.isEmpty list then
-    []
-  else
-    List.take n list :: chunk n (List.drop n list)
+    if List.isEmpty list then
+        []
+    else
+        List.take n list :: chunk n (List.drop n list)
+
 
 
 -- View
+
 
 main : Html msg
 main =
@@ -48,23 +95,25 @@ main =
 
 calendarDay : Date -> Html msg
 calendarDay date =
-  td []
-      [ text <| toString (day date) ]
+    td []
+        [ text <| toString (day date) ]
 
-calendarRow : (List Date) -> Html msg
+
+calendarRow : List Date -> Html msg
 calendarRow dateRow =
-  tr []
-      (List.map calendarDay dateRow)
+    tr []
+        (List.map calendarDay dateRow)
+
 
 calendarView : Model -> Html msg
 calendarView model =
-  let
-    weeks = model.currentDates |> chunk model.dayPerWeek
-    _ = Debug.log "weeks" weeks
-  in
-  div [ id "calendar" ]
-    [ h1 []
-        [ text "October" ]
-    , table []
-        (List.map calendarRow weeks)
-    ]
+    let
+        weeks =
+            model.currentDates |> chunk dayPerWeek
+    in
+        div [ id "calendar" ]
+            [ h1 []
+                [ text <| getMonthNameByIndex model.selectedMonthIndex ]
+            , table []
+                (List.map calendarRow weeks)
+            ]
