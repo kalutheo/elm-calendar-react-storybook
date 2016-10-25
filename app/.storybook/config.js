f00de978f202cc5a434348bc1120f72302b7e4a5
Theophile Kalumbu <theophile@ouicar.fr>
2016-10-25 14:11:43 +0200

    add new storybook

--- app/.storybook/config.js
+++ app/.storybook/config.js
@@ -1,8 +1,7 @@
 import { configure } from '@kadira/storybook';
 
 function loadStories() {
-  require('../stories/index.js');
-  // You can require as many stories as you need.
+  require('../stories');
 }
 
 configure(loadStories, module);
